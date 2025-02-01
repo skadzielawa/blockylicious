3f4a9e3a1312986358b6ec5b9932e00149a53c56
Szymon Kądzielawa <skadzielawa@gmail.com>
2025-02-01 17:16:50 +0100

    Implement block gap for clicky group

--- src/blocks/clickyButton/edit.js
+++ src/blocks/clickyButton/edit.js
@@ -1,8 +1,24 @@
-import { useBlockProps } from "@wordpress/block-editor";
+import { RichText, useBlockProps } from "@wordpress/block-editor";
 
 import "./editor.scss";
 
-export default function Edit() {
+export default function Edit(props) {
 	const blockProps = useBlockProps();
-	return <div {...blockProps}>Clicky Button</div>;
+	return (
+		<div {...blockProps}>
+			<RichText
+				placeholder="Label text"
+				value={props.attributes.labelText}
+				allowedFormats={[]}
+				multiline={false}
+				onSplit={() => {}}
+				onReplace={() => {}}
+				onChange={(newValue) => {
+					props.setAttributes({
+						labelText: newValue,
+					});
+				}}
+			/>
+		</div>
+	);
 }
--- src/blocks/clickyGroup/edit.js
+++ src/blocks/clickyGroup/edit.js
@@ -3,11 +3,15 @@ import {
 	useBlockProps,
 	useInnerBlocksProps,
 } from "@wordpress/block-editor";
-
+import { parseValue } from "../../utils/parseValue";
 import "./editor.scss";
 
-export default function Edit() {
-	const blockProps = useBlockProps();
+export default function Edit(props) {
+	console.log(props);
+	const blockGap = parseValue(props.attributes.style?.spacing?.blockGap || "");
+	const blockProps = useBlockProps({
+		style: { gap: blockGap },
+	});
 	const innerBlocksProps = useInnerBlocksProps(blockProps, {
 		template: [["blockylicious/clicky-button"]],
 		allowedBlocks: ["blockylicious/clicky-button"],
