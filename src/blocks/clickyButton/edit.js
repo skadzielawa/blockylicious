fa1f4362ae995517132ae0811a60e8187bcc9a0b
Szymon Kądzielawa <skadzielawa@gmail.com>
2025-02-01 18:28:06 +0100

    adding post types to js constant

--- src/blocks/clickyButton/edit.js
+++ src/blocks/clickyButton/edit.js
@@ -1,8 +1,18 @@
 import { RichText, useBlockProps } from "@wordpress/block-editor";
+import { useSelect } from "@wordpress/data";
 
 import "./editor.scss";
 
 export default function Edit(props) {
+	const postTypes = useSelect((select) => {
+		const data = select("core").getEntityRecords("root", "postType", {
+			per_page: -1,
+		});
+		return data?.filter(
+			(item) => item.visibility.show_in_nav_menus && item.visibility.show_ui,
+		);
+	});
+	console.log(postTypes);
 	const blockProps = useBlockProps();
 	return (
 		<div {...blockProps}>
