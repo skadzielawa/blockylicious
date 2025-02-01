8405a615e2be6a1d0d5b28f413474d62926fcf83
Szymon Kądzielawa <skadzielawa@gmail.com>
2025-02-01 18:10:27 +0100

    Justify content for Clicky Group

--- src/blocks/clickyGroup/edit.js
+++ src/blocks/clickyGroup/edit.js
@@ -1,21 +1,36 @@
 import {
-	InnerBlocks,
 	useBlockProps,
 	useInnerBlocksProps,
+	BlockControls,
+	JustifyContentControl,
 } from "@wordpress/block-editor";
 import { parseValue } from "../../utils/parseValue";
 import "./editor.scss";
 
 export default function Edit(props) {
-	console.log(props);
 	const blockGap = parseValue(props.attributes.style?.spacing?.blockGap || "");
 	const blockProps = useBlockProps({
-		style: { gap: blockGap },
+		style: { gap: blockGap, justifyContent: props.attributes.justifyContent },
 	});
 	const innerBlocksProps = useInnerBlocksProps(blockProps, {
 		template: [["blockylicious/clicky-button"]],
 		allowedBlocks: ["blockylicious/clicky-button"],
 	});
 
-	return <div {...innerBlocksProps}></div>;
+	return (
+		<>
+			<BlockControls>
+				<JustifyContentControl
+					allowedControls={["left", "center", "right"]}
+					value={props.attributes.justifyContent}
+					onChange={(newValue) => {
+						props.setAttributes({
+							justifyContent: newValue,
+						});
+					}}
+				/>
+			</BlockControls>
+			<div {...innerBlocksProps} />
+		</>
+	);
 }
