a9d3b4c9a4f927c596a0359e99c4ba512de6bb38
Szymon Kądzielawa <skadzielawa@gmail.com>
2025-02-01 16:26:12 +0100

    stop tracking build files

--- build/blocks/clickyGroup/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '656861bba97abc05ed23');
--- build/blocks/curvy/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b7e884f9deb6ad126f92');
