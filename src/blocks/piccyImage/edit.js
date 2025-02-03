e911ef69ca875c770f12ebad0b6547bc634f2163
Szymon Kądzielawa <skadzielawa@gmail.com>
2025-02-03 15:13:54 +0100

    saving and getting the image

--- src/blocks/piccyImage/edit.js
+++ src/blocks/piccyImage/edit.js
@@ -6,11 +6,30 @@ import {
 import { __ } from "@wordpress/i18n";
 import "./editor.scss";
 import metadata from "./block.json";
+import { useSelect } from "@wordpress/data";
 
 export default function Edit(props) {
 	const blockProps = useBlockProps();
+	const image = useSelect(
+		(select) => {
+			const data = select("core").getEntityRecord(
+				"postType",
+				"attachment",
+				props.attributes.imageId,
+			);
+			return data;
+		},
+		[props.attributes.imageID],
+	);
+
 	return (
 		<div {...blockProps}>
+			{!!props.attributes.imageId && !!image?.source_url && (
+				<img
+					style={{ height: 150, width: "100%", objectFit: "cover" }}
+					src={image.source_url}
+				/>
+			)}
 			<MediaUploadCheck>
 				<MediaUpload
 					allowedTypes={["image"]}
@@ -20,6 +39,12 @@ export default function Edit(props) {
 								{__("Select an image", metadata.textdomain)}
 							</button>
 						);
+					}}
+					value={props.setAttributes.imageId}
+					onSelect={(item) => {
+						props.setAttributes({
+							imageId: item.id,
+						});
 					}}
 				/>
 			</MediaUploadCheck>
