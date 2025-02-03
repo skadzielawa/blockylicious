985f3d03c65bf2c830a7ae2e04ed46f2210c1fe2
Szymon Kądzielawa <skadzielawa@gmail.com>
2025-02-03 13:14:26 +0100

    edit & preview button logic, added styles for edit mode

--- src/blocks/piccyGallery/edit.js
+++ src/blocks/piccyGallery/edit.js
@@ -1,3 +1,51 @@
+import {
+	BlockControls,
+	useBlockProps,
+	InnerBlocks,
+} from "@wordpress/block-editor";
+import { ToolbarGroup, ToolbarButton, Icon } from "@wordpress/components";
+import { __ } from "@wordpress/i18n";
+import metadata from "./block.json";
+import { useState } from "@wordpress/element";
+import "./editor.scss";
+
 export default function Edit(props) {
-	return null;
+	const blockProps = useBlockProps();
+	const [editMode, setEditMode] = useState(true);
+	return (
+		<>
+			<div {...blockProps}>
+				{!!editMode && (
+					<div className="edit-mode">
+						<span className="piccy-label">
+							{__("Piccy image gallery", metadata.textdomain)}
+						</span>
+						<InnerBlocks />
+					</div>
+				)}
+				{!editMode && <div>preview mode</div>}
+			</div>
+			<BlockControls>
+				<ToolbarGroup>
+					<ToolbarButton
+						icon={
+							editMode ? (
+								<Icon icon="welcome-view-site" />
+							) : (
+								<Icon icon="edit" />
+							)
+						}
+						label={
+							editMode
+								? __("Preview Gallery", metadata.textdomain)
+								: __("Edit Gallery", metadata.textdomain)
+						}
+						onClick={() => {
+							setEditMode((prevState) => !prevState);
+						}}
+					/>
+				</ToolbarGroup>
+			</BlockControls>
+		</>
+	);
 }
