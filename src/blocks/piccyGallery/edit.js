9967d1f38a26c5d2318715d902917489d968de3d
Szymon Kądzielawa <skadzielawa@gmail.com>
2025-02-03 17:14:11 +0100

    displaying images in preview mode

--- src/blocks/piccyGallery/edit.js
+++ src/blocks/piccyGallery/edit.js
@@ -7,7 +7,9 @@ import { ToolbarGroup, ToolbarButton, Icon } from "@wordpress/components";
 import { __ } from "@wordpress/i18n";
 import metadata from "./block.json";
 import { useState } from "@wordpress/element";
+import { useSelect } from "@wordpress/data";
 import "./editor.scss";
+import { ImageThumbnail } from "../../components/ImageThumbnail";
 
 export default function Edit(props) {
 	const blockProps = useBlockProps();
@@ -20,6 +22,15 @@ export default function Edit(props) {
 		},
 	);
 	const [editMode, setEditMode] = useState(true);
+	const innerBlocks = useSelect(
+		(select) => {
+			const { getBlocksByClientId } = select("core/block-editor");
+			const block = getBlocksByClientId(props.clientId)?.[0];
+			return block?.innerBlocks;
+		},
+		[props.clientId],
+	);
+
 	return (
 		<>
 			<div {...blockProps}>
@@ -31,7 +42,16 @@ export default function Edit(props) {
 						<div {...innerBlocksProps} />
 					</div>
 				)}
-				{!editMode && <div>preview mode</div>}
+				{!editMode && (
+					<div>
+						{(innerBlocks || []).map((innerBlock) => (
+							<ImageThumbnail
+								key={innerBlock.clientId}
+								imageId={innerBlock.attributes.imageId}
+							/>
+						))}
+					</div>
+				)}
 			</div>
 			<BlockControls>
 				<ToolbarGroup>
