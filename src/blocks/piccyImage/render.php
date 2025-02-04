830fa961abecb87c3389f09a03350fde8ba00d93
Szymon Kądzielawa <skadzielawa@gmail.com>
2025-02-04 11:56:11 +0100

    no image set support

--- src/blocks/piccyImage/render.php
+++ src/blocks/piccyImage/render.php
@@ -1,7 +1,10 @@
 <?php
-$block_wrapper_attributes = get_block_wrapper_attributes();
-$image_uri = wp_get_attachment_image_url( $attributes['imageId'] );
-$image_uri_large = wp_get_attachment_image_url( $attributes['imageId'], 'large' );
+$image_exists = $attributes['imageId'] ?? null;
+$block_wrapper_attributes = get_block_wrapper_attributes([
+	'style' => 'display:' . ( $image_exists ? 'block' : 'none' )
+]);
+$image_uri = wp_get_attachment_image_url( $attributes['imageId'] ?? 0);
+$image_uri_large = wp_get_attachment_image_url( $attributes['imageId'] ?? 0, 'large' );
 ?>
 
 <div <?php echo $block_wrapper_attributes; ?>>
