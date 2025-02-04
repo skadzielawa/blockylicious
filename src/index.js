8fe499365f8d3dd9ea2b8878a36c57cccb33c16f
Szymon Kądzielawa <skadzielawa@gmail.com>
2025-02-04 13:00:13 +0100

    register and apply the new text format

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("hello index");
+import { registerFormatType, applyFormat } from "@wordpress/rich-text";
+import { __ } from "@wordpress/i18n";
+import { RichTextToolbarButton } from "@wordpress/block-editor";
+
+registerFormatType("blockylicious/low-highlight", {
+	title: __("Low Highlight", "blockylicious"),
+	tagName: "span",
+	className: "blockylicious-low-highlight",
+	edit: ({ onChange, value }) => {
+		return (
+			<RichTextToolbarButton
+				icon={<div>test</div>}
+				title={__("Low Highlight", "blockylicious")}
+				onClick={() => {
+					onChange(
+						applyFormat(value, {
+							type: "blockylicious/low-highlight",
+						}),
+					);
+				}}
+			/>
+		);
+	},
+});
